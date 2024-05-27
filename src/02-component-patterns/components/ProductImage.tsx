3dcc59b8f64126f4c884ee58e28c9f771e2bb154
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2024-05-27 17:54:48 -0300

    Component Extend Styles listo

--- src/02-component-patterns/components/ProductImage.tsx
+++ src/02-component-patterns/components/ProductImage.tsx
@@ -4,8 +4,13 @@ import { ProductContext } from "./ProductCard";
 import styles from "../styles/styles.module.css";
 import noImage from "../assets/no-image.jpg";
 
+export interface Props {
+  img?: string;
+  className?: string;
+  style?: React.CSSProperties;
+}
 
-export const ProductImage = ({ img }: { img?: string }) => {
+export const ProductImage = ({ img, className, style }: Props) => {
   const { product } = useContext(ProductContext);
 
   let imgToShow: string;
@@ -19,6 +24,11 @@ export const ProductImage = ({ img }: { img?: string }) => {
   }
 
   return (
-    <img className={styles.productImg} src={imgToShow} alt="Product image" />
+    <img
+      className={`${styles.productImg} ${className}`}
+      style={style}
+      src={imgToShow}
+      alt="Product image"
+    />
   );
 };
--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -4,6 +4,7 @@ import {
   ProductImage,
   ProductTitle,
 } from "../components";
+import "../styles/custom-styles.css";
 
 const product = {
   id: "1",
@@ -17,16 +18,33 @@ export const ShoppingPage = () => {
       <h1>Shopping Store</h1>
       <hr />
       <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
-        <ProductCard product={ product }>
-          <ProductCard.Image />
-          <ProductCard.Title title={ product.title } />
-          <ProductCard.Buttons />
+        <ProductCard product={product} className="bg-dark text-white">
+          <ProductCard.Image className="custom-image" />
+          <ProductCard.Title title={product.title} className="text-bold" />
+          <ProductCard.Buttons className="custom-buttons" />
         </ProductCard>
 
-        <ProductCard product={product}>
-          <ProductImage />
-          <ProductTitle title="Hola Mundo" />
-          <ProductButtons />
+        <ProductCard product={product} className="bg-dark text-white">
+          <ProductImage
+            className="custom-image"
+            style={{ boxShadow: "10px 10px 10px rgba(0, 0, 0, 0.2)" }}
+          />
+          <ProductTitle className="text-bold" />
+          <ProductButtons className="custom-buttons" />
+        </ProductCard>
+
+        <ProductCard
+          product={product}
+          style={{
+            backgroundColor: "#70D1F8",
+          }}
+        >
+          <ProductImage
+            className="custom-image"
+            style={{ boxShadow: "10px 10px 10px rgba(0, 0, 0, 0.2)" }}
+          />
+          <ProductTitle style={{ fontWeight: "bold" }} />
+          <ProductButtons style={{ display: "flex", justifyContent: "end" }} />
         </ProductCard>
       </div>
     </div>
