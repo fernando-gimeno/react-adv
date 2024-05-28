8098cc2dcdb647269df3047c22278d64746c08fc
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2024-05-27 23:05:16 -0300

    Control props alt

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { OnChangeArgs, Product } from "../interfaces/interfaces";
 
 interface UseProductArgs {
@@ -14,17 +14,9 @@ export const useProduct = ({
 }: UseProductArgs) => {
   const [counter, setCounter] = useState(value);
 
-  const isControlled = useRef(!!onChange);
-
   const increaseBy = (value: number) => {
-    if (isControlled.current && onChange) {
-      return onChange({ count: value, product });
-    }
-
     const newValue = Math.max(counter + value, 0);
-
     setCounter(newValue);
-
     onChange && onChange({ count: newValue, product });
   };
 
--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -13,41 +13,25 @@ export const useShoppingCart = () => {
     count: number;
     product: Product;
   }) => {
+    console.log({ count });
+    
     setShoppingCart((prevShoppingCart) => {
-      const productInCart: ProductInCart = prevShoppingCart[product.id] || {
-        ...product,
-        count: 0,
-      };
+      if (count === 0) {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { [product.id]: _, ...rest } = prevShoppingCart;
 
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
         return {
-          ...prevShoppingCart,
-          [product.id]: productInCart,
+          ...rest,
         };
       }
 
-      // Borrar el producto
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const { [product.id]: _, ...rest } = prevShoppingCart;
-      return { ...rest };
-
-      // if (count === 0) {
-      //   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      //   const { [product.id]: _, ...rest } = prevShoppingCart;
-
-      //   return {
-      //     ...rest,
-      //   };
-      // }
-
-      // return {
-      //   ...prevShoppingCart,
-      //   [product.id]: {
-      //     ...product,
-      //     count,
-      //   },
-      // };
+      return {
+        ...prevShoppingCart,
+        [product.id]: {
+          ...product,
+          count,
+        },
+      };
     });
   };
 
